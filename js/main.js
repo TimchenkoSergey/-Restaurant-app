0a9283cc55e9230ea4e13a476f40441b8ce5f586
Sergey Timchenko <art-street93@mail.ru>
2016-09-22 14:08:22 +0300

    Added a transition to meal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 angular.module("Meals", [])
 
 .factory("mealsFactory", function ($http, $q) {
-	let mealApiUrl = "meals.json",
-		meals      = null,
-		currency   = "";
+	let mealApiUrl        = "meals.json",
+		meals             = null,
+		currency          = "",
+		currentMeal       = null,
+		currentMealStatus = "new",
+		currentAmount     = 1,
+		openMeal          = false;
 
 	return {
+		getOpenMeal : function () {
+			return openMeal;
+		},
+
+		setOpenMeal : function (flag) {
+			openMeal = flag;
+		},
 
 		getMeals : function () {
 			let deferred = $q.defer();
@@ -25,16 +36,84 @@ angular.module("Meals", [])
 				});
 
 			return deferred.promise;
+		},
+
+		setCurrentMeal : function (meal) {
+			currentMeal = meal;
+		},
+
+		getCurrentMeal : function () {
+			return currentMeal;
+		},
+
+		setCurrentMealStatus : function (status) {
+			currentMealStatus = status;
+		},
+
+		getCurrentMealStatus : function () {
+			return currentMealStatus;
+		},
+
+		getCurrency : function () {
+			return currency;
+		},
+
+		setCurrentMealAmount : function (amount) {
+			currentAmount = amount;
+		},
+
+		getCurrentMealAmount : function () {
+			return currentAmount;
 		}
 	};
 })
 
 .controller("mealsListCtrl", function ($scope, mealsFactory) {
-
+	$scope.open = mealsFactory.getOpenMeal();
+	
 	mealsFactory.getMeals()
 		.then(function (mealsObj) {
 			$scope.currency = mealsObj.currency;
 			$scope.products = mealsObj.products;
 	});
-	console.log("lololol");
+
+	$scope.openMeal = function (meal) {
+		mealsFactory.setCurrentMeal(meal);
+		mealsFactory.setCurrentMealStatus("new");
+		mealsFactory.setCurrentMealAmount(1);
+		mealsFactory.setOpenMeal(true);
+		$scope.open = mealsFactory.getOpenMeal();
+	};
+})
+
+.directive("meal",function (mealsFactory) {
+	return {
+		restrict : "E",
+		replace : true,
+		templateUrl : "meal.html",
+		scope : {},
+		controller : function ($scope) {
+			$scope.currentMeal  = mealsFactory.getCurrentMeal();
+			$scope.currency     = mealsFactory.getCurrency();
+			$scope.selectAmount = mealsFactory.getCurrentMealAmount();
+			$scope.mealStatus   = mealsFactory.getCurrentMealStatus();
+			$scope.open         = mealsFactory.getOpenMeal();
+			
+			$scope.cencel = function () {
+				
+			}
+			
+			$scope.removeMeal = function () {
+				
+			}
+			
+			$scope.addMeal = function () {
+				
+			}
+			
+			$scope.saveMeal = function () {
+				
+			}
+		}
+	};
 });
