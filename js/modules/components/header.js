f5f9e4a4c995feaa7ce4163b24952a7a59fd0b95
Sergey Timchenko <art-street93@mail.ru>
2016-10-04 21:34:37 +0300

    *header* translated into component syntax.

---
+++ js/modules/components/header.js
@@ -0,0 +1,8 @@
+app.component("headerPage", {
+    restrict : "E",
+    replace : true,
+    templateUrl : "component_templates/headerPage.html",
+    bindings : {},
+    controllerAs : "header",
+    controller : function () {}
+});
