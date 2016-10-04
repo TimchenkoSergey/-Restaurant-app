8cc95f0945c17c5c76976713560f45456f266495
Sergey Timchenko <art-street93@mail.ru>
2016-10-04 21:28:44 +0300

    *menu* translated into component syntax.

---
+++ js/modules/components/menu.js
@@ -0,0 +1,8 @@
+app.component("menuPage", {
+    restrict : "E",
+    replace : true,
+    templateUrl : "component_templates/menuPage.html",
+    bindings : {},
+    controllerAs : "menu",
+    controller : function () {}
+});
