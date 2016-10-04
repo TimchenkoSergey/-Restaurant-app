3f9ce9e825d9194de7b6bdb6e35732e677c223cd
Sergey Timchenko <art-street93@mail.ru>
2016-10-04 21:42:22 +0300

    *footer* translated into component syntax.

---
+++ js/modules/components/footer.js
@@ -0,0 +1,8 @@
+app.component("footerPage", {
+    restrict : "E",
+    replace : true,
+    templateUrl : "component_templates/footerPage.html",
+    bindings : {},
+    controllerAs : "footer",
+    controller : function () {}
+});
